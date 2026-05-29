62f1b9853a3c60a356ffbc4975c6433606e9abe8
jordankitio-repo <jordangardenkitio@gmail.com>
2026-05-29 14:41:10 -0500

    fix: add barcode format support for EAN and UPC scanning

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useRef } from 'react'
-import { Html5Qrcode } from 'html5-qrcode'
+import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode'
 
 function BarcodeScanner({ onDetected, onClose }) {
   const scannerRef = useRef(null)
 
   useEffect(() => {
-    const scanner = new Html5Qrcode('barcode-reader')
+    const formatsToSupport = [
+  Html5QrcodeSupportedFormats.EAN_13,
+  Html5QrcodeSupportedFormats.EAN_8,
+  Html5QrcodeSupportedFormats.UPC_A,
+  Html5QrcodeSupportedFormats.UPC_E,
+  Html5QrcodeSupportedFormats.CODE_128,
+  Html5QrcodeSupportedFormats.QR_CODE,
+]
+
+const scanner = new Html5Qrcode('barcode-reader', { formatsToSupport })
     scannerRef.current = scanner
 
     scanner.start(
